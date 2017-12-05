6fe71313030f6ae8d22e5477bb021474695fad17
sophiazhao <sophiazhao1210@gmail.com>
2017-12-04 20:46:19 -0500

    ship

--- assets/js/components/tile.js
+++ assets/js/components/tile.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
 class Tile extends React.Component {
+
+   constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <td onClick={this.props.onClick}></td>
+      <td onClick={() =>{
+         window.alert("Invalid Ship");
+       }}>
+        {this.state.value}
+      </td>
     );
   };
-
-  handleClick() {
-    window.alert("Invalid ship!");
-  }
 }
 
 export default Tile
